33498c7d5a656c5d896ace5d28c09f0816582238
kmss6905 <kmss69052@naver.com>
2023-04-10 21:47:27 +0900

    release: 2.0

--- src/components/AppHero.vue
+++ src/components/AppHero.vue
@@ -3,7 +3,7 @@
   <div class="hero min-h-screen">
     <div class="hero-content text-center">
       <div class="max-w-md">
-        <h1 class="text-5xl font-bold">할일 공유 플랫폼</h1>
+        <h1 class="text-5xl font-bold">할일 공유 플랫폼<span class="text-xs">{{ appVersion }}</span></h1>
         <p class="py-6">
           "할일을 공유해보세요!" 웹사이트에서 효율적으로 일상적인 과제를 완수하세요. 다른 이들과 함께 공유하고 협력하여 문제를 해결할 수 있습니다.
         </p>
@@ -23,10 +23,12 @@ export default defineComponent({
       name: 'AppHero',
       data() {
         return {
-          isCurrentUserExist: false
+          isCurrentUserExist: false,
+          appVersion: "",
         }
       },
       mounted() {
+        this.appVersion = process.env.VUE_APP_VERSION;
         const currentUser = localStorage.getItem('currentUser');
         if (currentUser === null) {
           this.isCurrentUserExist = true;
